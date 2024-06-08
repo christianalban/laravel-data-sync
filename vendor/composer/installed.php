6e13747c49f4fa8278a4c95f45dc4155f7c889ff
Christian Albán <christianalbanctdb1d@gmail.com>
2024-06-08 13:34:28 -0500

    Initial commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'alban/laravel-data-sync',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '72a94e06351e964417d088509ef8e9d0b3a16a8e',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'alban/laravel-data-sync' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '72a94e06351e964417d088509ef8e9d0b3a16a8e',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
